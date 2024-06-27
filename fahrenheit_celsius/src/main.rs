6aa0cc41965af0eb09bfc226ed7c63144aa671a7
mizotii <omphen@uw.edu>
2024-06-27 15:22:55 -0700

    finish

--- fahrenheit_celsius/src/main.rs
+++ fahrenheit_celsius/src/main.rs
@@ -5,18 +5,23 @@ fn main() {
     println!("F -> C calculator");
 
     // input
-    println!("please input a valid temperature in F.");
+    loop {
+        println!("please input a valid temperature in F.");
+    
+        let mut input = String::new();
+    
+        io::stdin()
+            .read_line(&mut input)
+            .expect("failed to read input");
+    
+        let input: f32 = match input.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
-    let mut input = String::new();
-
-    io::stdin()
-        .read_line(&mut input)
-        .expect("failed to read input");
-
-    let input: u32 = match input.trim().parse() {
-        Ok(num) => num,
-        Err(_) => println!("{}", error),
-    };
-
-    println!("input: {input}");
+        let celsius = (input - 32.0) * (5.0 / 9.0);
+    
+        println!("the temperature: {input}, in fahrenheit, is equal to the temperature: {celsius}, in celsius");
+        break;
+    }
 }
