1a9b5315906e00b5e34a97ae9a0542690cb742ab
mizotii <omphen@uw.edu>
2024-07-03 18:08:31 -0700

    progress

--- chapter_6/ip_address/src/main.rs
+++ chapter_6/ip_address/src/main.rs
@@ -1,6 +1,30 @@
-enum IpAddrKind {
-    V4,
-    V6,
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
+impl Message {
+    fn call(&self) {
+
+    }
+}
+
+let m = Message::Write(String::from("hello"));
+m.call();
+
+let home = IpAddr::V4(127, 0, 0, 1);
+
+let loopback = IpAddr::V6(String::from("::1"));
+
+fn route(ip_kind: IpAddrKind) {
+
 }
 
 fn main() {
