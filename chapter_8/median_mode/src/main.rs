b416dac5f80a289e83d8213944d5a8cf5a913da3
mizotii <omphen@uw.edu>
2024-07-07 20:59:14 -0700

    continue work on median

--- chapter_8/median_mode/src/main.rs
+++ chapter_8/median_mode/src/main.rs
@@ -8,12 +8,10 @@ const MAX_DATA: usize = 100;
 fn main() {
     let length = rand::thread_rng().gen_range(MIN_LENGTH..=MAX_LENGTH);
     let list = generate_list(&length);
+    println!("the list i generated is here: {list:?}");
 
-    println!("{list:?}");
-
-    let median = find_median(&list, &length);
-
-    let mode = find_mode(&list);
+    let median = find_median(list, length);
+    println!("the median of this list is: {median}");
 }
 
 fn generate_list(length: &usize) -> Vec<usize> {
@@ -25,37 +23,41 @@ fn generate_list(length: &usize) -> Vec<usize> {
     list
 }
 
-fn find_median(list: &Vec<usize>, length: &usize) -> f32 {
-    if length % 2 != 1 {
-        quickselect(&list, length / 2, get_pivot(length))
+fn find_median(list: Vec<usize>, length: usize) -> f32 {
+    let mut pivot_index = get_pivot(length);
+    if length % 2 == 0 {
+        (quickselect(&list, (length - 1) / 2, pivot_index) + quickselect(&list, ((length - 1) / 2) + 1, pivot_index)) / 2.0
     } else {
-        (quickselect(&list, length / 2, get_pivot(length)) + quickselect(&list, (length / 2) + 1, get_pivot(length))) / 2.0
+        quickselect(&list, (length - 1) / 2, pivot_index)
     }
 }
 
-fn find_mode(list: &Vec<usize>) -> usize {
-    1
-}
-
-fn quickselect(list: &Vec<usize>, k: usize, pivot_index: usize) -> f32 {
-    let pivot: &usize = &list[pivot_index];
-    let mut lesser_list = Vec::new();
-    let mut greater_list = Vec::new();
-    for i in list {
-        if i <= pivot {
-            lesser_list.push(*i);
+fn quickselect(list: &Vec<usize>, mut k: usize, pivot_index: usize) -> f32 {
+    // base case
+    if list.len() == 1 {
+        list[0] as f32
+    } else {
+        // recurse
+        let mut lesser_list: Vec<usize> = Vec::new();
+        let mut greater_list: Vec<usize> = Vec::new();
+        let pivot = list[pivot_index];
+        for i in list {
+            if i <= &pivot {
+                lesser_list.push(*i);
+            } else {
+                greater_list.push(*i);
+            }
+        }
+        println!("{pivot} {lesser_list:?} {greater_list:?}");
+        let converted_len_lesser = lesser_list.len() - 1;
+        if &converted_len_lesser >= &k {
+            quickselect(&lesser_list, k, get_pivot(converted_len_lesser))
         } else {
-            greater_list.push(*i);
+            quickselect(&greater_list, k - &converted_len_lesser, get_pivot(greater_list.len()))
         }
     }
-    if lesser_list.len() >= k {
-        quickselect(&lesser_list, k, get_pivot(lesser_list.len()))
-    } else {
-        0.0
-    }
-    0.0
 }
 
-fn get_pivot(length: &usize) -> usize {
-    rand::thread_rng().gen_range(0..*length)
+fn get_pivot(length: usize) -> usize {
+    rand::thread_rng().gen_range(0..length)
 }
