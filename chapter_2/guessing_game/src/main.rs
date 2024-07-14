f9c21b1c63d00d1c1f7ef7930a561c54911c3ae1
mizotii <omphen@uw.edu>
2024-07-13 18:48:13 -0700

    impl guess type

--- chapter_2/guessing_game/src/main.rs
+++ chapter_2/guessing_game/src/main.rs
@@ -1,6 +1,24 @@
 use rand::Rng;
 use std::{cmp::Ordering, io};
 
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("guess value must be between 1 and 100, got {value}");
+        }
+
+        Guess { value }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
+
 fn main() {
     println!("guess the number!");
 
@@ -15,10 +33,15 @@ fn main() {
             .read_line(&mut guess)
             .expect("failed to read line");
     
-        let guess: u32 = match guess.trim().parse() {
+        let guess: i32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
+
+        if guess < 1 || guess > 100 {
+            println!("the secret number is between 1 and 100.");
+            continue;
+        }
     
         println!("you guessed: {guess}");
     
