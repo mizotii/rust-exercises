552b41826d5649b2663d539297b305cf6db600ae
mizotii <omphen@uw.edu>
2024-07-15 14:00:13 -0700

    add tests

--- chapter_2/guessing_game/src/main.rs
+++ chapter_2/guessing_game/src/main.rs
@@ -7,8 +7,14 @@ pub struct Guess {
 
 impl Guess {
     pub fn new(value: i32) -> Guess {
-        if value < 1 || value > 100 {
-            panic!("guess value must be between 1 and 100, got {value}");
+        if value < 1 {
+            panic!(
+                "guess value must be greater than or equal to 1, got {value}"
+            );
+        } else if value > 100 {
+            panic!(
+                "guess value must be less than or equal to 100, got {value}"
+            );
         }
 
         Guess { value }
@@ -16,6 +22,17 @@ impl Guess {
 
     pub fn value(&self) -> i32 {
         self.value
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    #[should_panic(expected = "less than or equal to 100")]
+    fn greater_than_100() {
+        Guess::new(200);
     }
 }
 
